b47682cb8bd42abf491a774fec9715f1bc2b11bc
Rohan <ry7games@gmail.com>
2024-04-17 09:20:32 -0500

    completed day 1 of AoC 2023

---
+++ part1.py
@@ -0,0 +1,18 @@
+# Value found from each line of data.txt file
+sumArray = []
+
+
+with open('Advent of Code/Day 1/data.txt', 'r') as file:
+    for line in file:
+        charArray = []
+        # Separate line of file in array of characters
+        for character in line:
+            if character.isnumeric():
+                charArray.append(character)
+        solution = int(charArray[0] + charArray[-1])
+        sumArray.append(solution)
+
+
+# Takes sum of all number 
+print(sum(sumArray))
+
