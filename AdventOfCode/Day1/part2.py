09e22de5f60a03b1bc5436d434d80a9ec3db41c8
Rohan <ry7games@gmail.com>
2024-04-18 10:02:05 -0500

    Update part2.py
    Fixed problem, which was the edge case:
    'eightwo'.
    
    To fix, we added both the first and last letter of the word to be replaced into the new string with the digit.

--- AdventOfCode/Day1/part2.py
+++ AdventOfCode/Day1/part2.py
@@ -4,7 +4,7 @@ nums = ["zero", "one","two","three","four", "five", "six", "seven", "eight", "ni
 def replaceLine(t):
     for i in range(0, 2):
         for j  in range(0, len(nums)):
-            t = t.replace(nums[j], str(j) + nums[j][-1])
+            t = t.replace(nums[j], nums[j][0] + str(j) + nums[j][-1])
     return t
 
 with open('AdventOfCode\Day1\data.txt', 'r') as file:
@@ -18,7 +18,7 @@ with open('AdventOfCode\Day1\data.txt', 'r') as file:
             if character.isnumeric():
                 charArray.append(character)
         solution = int(charArray[0] + charArray[-1])
-        print(f'{i} : {line} : {solution}')
+        print(f'{i}\n:  {convStr} : {line} : {solution}')
         sum = sum + solution
 
 print(sum)
